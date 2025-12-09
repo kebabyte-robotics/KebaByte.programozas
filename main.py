48712516107045027f865525292da43962fe6996
KebaByte <kebabyte.robotics@gmail.com>
2025-12-09 06:59:03 +0100

    elore es hatra futas

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pybricks.pupdevices import *
 from pybricks.parameters import *
 from pybricks.robotics import *
 from pybricks.tools import *
+from umath import *
  
 hub = PrimeHub() #elnevezzük a PrimeHub()-ot hub változóra
 motorB = Motor(Port.E, Direction.COUNTERCLOCKWISE) #elnevezzük a Motort nagy_motorB változóra port, irány
@@ -17,6 +18,54 @@ db.settings(
     turn_acceleration=1920
 )
 hub.system.set_stop_button(Button.BLUETOOTH) #beállítja a stopgombot a bluetooth gombra
+kerek_atmero = 43.2 #kerék átmérője                     
+kerek_kerulet = pi * kerek_atmero  #a kerékkerületét kiszámítjuk, hogy fok->mm pi*kerék átmérő
+egyenes_ero = 5.6 #milyen erősen reagáljon az eltérésre: pl 2 fok az 2*5,6/2=5,6az egyik motor nagyobb másik kisebb                                      
+def forward(tavolsag, legkisebb_sebesseg, maximalis_sebesseg): 
+    kerek_motorB.reset_angle(0)  #bal motor foka 0               
+    kerek_motorJ.reset_angle(0)  #jobb motor foka 0              
+    hub.imu.reset_heading(0)   #gyro foka 0 ehhez képest lesz eltérés            
+    cel_fok = (360 / kerek_kerulet) * tavolsag #a teljes köből kiszámoljuk 56 az hány fok majd megszolozzuk a távolsággal
+    while True: #örökre megy amig nem lépünk ki
+        bal_fok = abs(kerek_motorB.angle())  #mennyit fordult a bal motor, kell abs mert lehet -   
+        jobb_fok = abs(kerek_motorJ.angle())  #mennyit fordult a jobb motor, kell abs mert lehet -
+        atlag_fok = (bal_fok + jobb_fok) / 2 #mennyi az átlaguk, kicsi eltérés miatt
+        haladas = atlag_fok / cel_fok #haladás aránya 0-1
+        if haladas < 0.3: #ha az arány kisebb 0.3-nál, alsó 30%
+            aktualis_sebesseg = legkisebb_sebesseg + (maximalis_sebesseg - legkisebb_sebesseg) * (haladas / 0.3) #alap a legkisebb, megmondja + mennyi, 0-0.3 arány
+        elif haladas > 0.75: #ha az arány nagyobb 0.75-nél, felső 25%
+            aktualis_sebesseg = legkisebb_sebesseg + (maximalis_sebesseg - legkisebb_sebesseg) * ((1 - haladas) / 0.25) #alap a legkisebb, megmondja + mennyi, lassítás miatt 1, 0.75-1 arány 
+        else: #ha 0.3-0.75
+            aktualis_sebesseg = maximalis_sebesseg #menjen max sebeséggel
+        irany_hiba = hub.imu.heading()     #mennyire fordult el az eredetitől
+        korrekcio = irany_hiba * egyenes_ero / 2 #fokot megszorozzuk 5,6 felével, ha minus balra fordult sokat, ha plus akkor jobbra
+        kerek_motorB.run(aktualis_sebesseg - korrekcio)  #lelassít a korekcióval
+        kerek_motorJ.run(aktualis_sebesseg + korrekcio) #gyorsít a korekcióval
+        if atlag_fok >= cel_fok: #azért >=, mert lehet negyed fokkal több
+            break            # kilép a ciklusból  
+
+def backward(tavolsag, legkisebb_sebesseg, maximalis_sebesseg): 
+    kerek_motorB.reset_angle(0)  #bal motor foka 0               
+    kerek_motorJ.reset_angle(0)  #jobb motor foka 0              
+    hub.imu.reset_heading(0)   #gyro foka 0 ehhez képest lesz eltérés            
+    cel_fok = (360 / kerek_kerulet) * tavolsag #a teljes köből kiszámoljuk 56 az hány fok majd megszolozzuk a távolsággal
+    while True: #örökre megy amig nem lépünk ki
+        bal_fok = abs(kerek_motorB.angle())  #mennyit fordult a bal motor, kell abs mert lehet -   
+        jobb_fok = abs(kerek_motorJ.angle())  #mennyit fordult a jobb motor, kell abs mert lehet -
+        atlag_fok = (bal_fok + jobb_fok) / 2 #mennyi az átlaguk, kicsi eltérés miatt
+        haladas = atlag_fok / cel_fok #haladás aránya 0-1
+        if haladas < 0.3: #ha az arány kisebb 0.3-nál, alsó 30%
+            aktualis_sebesseg = legkisebb_sebesseg + (maximalis_sebesseg - legkisebb_sebesseg) * (haladas / 0.3) #alap a legkisebb, megmondja + mennyi, 0-0.3 arány
+        elif haladas > 0.75: #ha az arány nagyobb 0.75-nél, felső 25%
+            aktualis_sebesseg = legkisebb_sebesseg + (maximalis_sebesseg - legkisebb_sebesseg) * ((1 - haladas) / 0.25) #alap a legkisebb, megmondja + mennyi, lassítás miatt 1, 0.75-1 arány 
+        else: #ha 0.3-0.75
+            aktualis_sebesseg = maximalis_sebesseg #menjen max sebeséggel
+        irany_hiba = hub.imu.heading()     #mennyire fordult el az eredetitől
+        korrekcio = irany_hiba * egyenes_ero / 2 #fokot megszorozzuk 5,6 felével
+        kerek_motorB.run(-aktualis_sebesseg + korrekcio)  #lelassít a korekcióval, minusz mert hátra megy
+        kerek_motorJ.run(-aktualis_sebesseg - korrekcio) #gyorsít a korekcióval, minusz mert előre megy
+        if atlag_fok >= cel_fok: #azért >=, mert lehet negyed fokkal több
+            break            # kilép a ciklusból  
  
 def futas_0():
     while True:
