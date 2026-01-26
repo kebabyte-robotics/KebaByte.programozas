72249afd801fe17776bf55d585f3a0d209a7c96e
kebabyte-robotics <kebabyte.robotics@gmail.com>
2026-01-26 07:45:33 +0100

    Update 2. Futás.py

--- 2. Futás.py
+++ 2. Futás.py
@@ -1,1 +1,11 @@
+from alapkod import *
 
+egyenes(61)
+kanyarodas(25)
+egyenes(339)
+kanyarodas(-25)
+egyenes(321, e_legkisebb_sebesseg=700, e_legnagyobb_sebesseg=1000)
+kanyarodas(-34)
+egyenes(29)
+kanyarodas(55)
+egyenes(21)
