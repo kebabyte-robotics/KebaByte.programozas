a2eb770b31186d0c462422c996b285ceedb80568
kebabyte-robotics <kebabyte.robotics@gmail.com>
2026-04-23 19:46:27 +0200

    Add motor stop commands before task execution
    Stop the motors before executing the next task.

--- Vegso.py
+++ Vegso.py
@@ -257,6 +257,10 @@ while True: #egy ciklus ami addig fut amig nem lépünk ki
         hub.imu.reset_heading(0) #a gyro értékét 0-ra állítjuk
         try: #futassd le a kódot, ha kilép menj az exceptre
             hub.system.set_stop_button(Button.CENTER) # a stop button legyen a középső gomb
+            bal.stop() #bal áljon le
+            jobb.stop() #jobb áljon le
+            feltet_bal.stop() #feltet_bal álljon le
+            feltet_jobb.stop() #feltet_jobb álljon le
             futasok[futas]() #futassa le a futasok futas elemét
             futas = (futas + 1) % max_futas #a futas egyen egyenlő a futas + 1nak a maradéka a max_futassal
         except SystemExit: #ha ki akar lépni ezt csináld
